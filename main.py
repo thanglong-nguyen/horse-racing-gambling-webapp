816b3bf5b07bc1dd1b3ee46cde649aafac06a78d
Thanh Long <thanhlongtomcua@gmail.com>
2026-07-11 13:15:15 +1000

    Remove pycache from tracking

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from models import *
 from renderer import *
+from utilities import RaceRenderer
 
 if __name__ == "__main__":
 
-    # --- Track setup ---
     # Finish node slightly off the track, all lanes connect to this
     finish = TrackNode(15, 0, -1, "Finish")
 
 
     track = RaceTrack(
         finish_node=finish,
-        lanes=8,
-        straight_length=300,
-        base_radius=100
+        straight_length=100, #500m straight
+        base_radius=20 #100m radius turns
     )
 
 
@@ -22,13 +21,13 @@ if __name__ == "__main__":
 
     # --- Horses ---
     # base_speed in m/s, stamina in [0,100+], stamina_loss_per_meter controls fade
-    h1 = Horse("Thunder", 20, 100, 0.05)   # solid, steady
-    h2 = Horse("Blaze",   28, 100, 0.04)   # fast, good stamina
-    h3 = Horse("Storm",   19, 100, 0.04)   # balanced
-    h4 = Horse("Falcon",  21,  95, 0.06)   # bursty, burns stamina faster
-    h5 = Horse("Shadow",  18, 110, 0.03)   # slower top speed, great stamina
-    h6 = Horse("Comet",   23,  90, 0.05)   # strong sprinter
-    h7 = Horse("Titan",   17, 120, 0.035)  # tanky grinder
+    h1 = Horse("Thunder", 22, 100, 0.05)   # solid, steady
+    h2 = Horse("Blaze",   22, 100, 0.04)   # fast, good stamina
+    h3 = Horse("Storm",   22, 100, 0.04)   # balanced
+    h4 = Horse("Falcon",  22,  100, 0.06)   # bursty, burns stamina faster
+    h5 = Horse("Shadow",  22, 110, 0.03)   # slower top speed, great stamina
+    h6 = Horse("Comet",   22,  100, 1)   # strong sprinter
+    h7 = Horse("Titan",   base_speed=22, stamina=90, stamina_loss_per_meter=0.04)  # tanky grinder
 
     horses_with_lanes = [
         (h1, 0),
@@ -45,8 +44,15 @@ if __name__ == "__main__":
     race = Race(
         horses_with_lanes=horses_with_lanes,
         track=track,
-        tick_dt=0.5,    # gives A* enough horizon to overtake smartly
-        total_time=12.0 # enough time for everyone to finish a ~120–170m lap
+        tick_dt= 1,    # gives A* enough horizon to overtake smartly
+        total_time= 1000 # enough time for everyone to finish a ~120–170m lap
     )
 
-    race.play()
+    # race.play()
+
+    race.play_replay(0.5)
+
+
+    # race.run()
+    # RaceRenderer.plot_paths(race, track)
+    
